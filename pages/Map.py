b2ece9567254a0fd603fedbb82d878824bd90df5
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-04-29 10:35:55 +0200

    DEV: test

--- pages/Map.py
+++ pages/Map.py
@@ -3,11 +3,6 @@ from ressources import HelperFunctions
 
 import folium
 import geopandas as gpd
-import rioxarray as riox
-import numpy as np
-import matplotlib.pyplot as plt
-import os
-
 from streamlit_folium import st_folium
 
 HelperFunctions.add_dlr_logo_to_page()
@@ -15,14 +10,11 @@ HelperFunctions.add_dlr_logo_to_page()
 # Map Title
 st.title("Map")
 
-
 #neighborhoods_berlin= gpd.read_file("test.gpkg")
 neighborhoods_berlin= gpd.read_file("./data/1 Land Prices/Land_Prices_Neighborhood_Berlin.gpkg")
-
 f = folium.Figure(width=1000, height=500)
 
 m = neighborhoods_berlin.explore(height=500, width=1000, name="Neighborhoods").add_to(f)
-
 folium.LayerControl().add_to(m)
 
 # call to render Folium map in Streamlit
