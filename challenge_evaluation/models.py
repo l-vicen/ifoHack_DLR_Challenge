2106b7dac27f8cb1644cc341297ff3c4dc4ec3ab
Lucas Vicentim <lucasvicentim0301@gmail.com>
2023-04-30 12:04:13 +0200

    DEV: TEst

--- challenge_evaluation/models.py
+++ challenge_evaluation/models.py
@@ -1,3 +1,9 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.decomposition import PCA
+import pandas as pd
+
 def model_fit(X_train, y_train):
     rf = RandomForestRegressor(n_estimators=100, max_depth=5)
     rf.fit(X_train, y_train)
